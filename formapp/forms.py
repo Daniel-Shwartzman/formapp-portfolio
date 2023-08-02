90e88946e560e7e2aa61c3842a1e0b88b970a939
Stav Bendarsky <sbendarsky@gmail.com>
2023-08-02 14:32:06 +0300

    backend

---
+++ formapp/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf.form import FlaskForm
+from wtforms.fields import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    TextAreaField,
+    TextAreaField
+)
+from wtforms.validators import (
+    DataRequired,
+    Length
+)
+from formapp.validators import Unique
+from models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(1, 30), Unique(User, User.username, message='Username already exists choose another.')]
+    )      
+    password = PasswordField('Password', validators=[DataRequired(), Length(4, 128)])
+    submit = SubmitField('Continue')
+
+class RegisterForm(FlaskForm):
+
+    username = StringField('Username', 
+        validators=[DataRequired(), Length(1, 30), Unique(User, User.username, message='Username already exists choose another.')]
+    )      
+    password = PasswordField('Password', validators=[DataRequired(), Length(4, 128)])
+    isOfficer = BooleanField('Officer', validators=[DataRequired()])
+    submit = SubmitField('Continue')
---
+++ formapp/models.py
@@ -0,0 +1,40 @@
+from flask import url_for
+from flask_login.mixins import UserMixin
+from werkzeug.security import(
+    generate_password_hash,
+    check_password_hash
+)
+from extensions import database as db
+
+class User(db.Model, UserMixin):
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer(38), primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    isOfficer = db.Column(db.Boolean, default=False, nullable=False)
+
+    # profile = db.Relationship('Profile', backref='user', cascade='save-update, merge, delete')
+
+
+    @classmethod
+    def get_user_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_officer(cls):
+        return cls.query.filter_by(isOfficer=True).first()
+
+    def __repr__(self):
+        return '<User> {}'.format(self.username)
