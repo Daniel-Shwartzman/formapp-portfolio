c203b022c5bb5a0b9aa05d5dd7fb07560415b23f
sbendarsky <sbendarsky@gmail.com>
2023-08-17 13:38:59 +0300

    update

--- formapp/extensions.py
+++ formapp/extensions.py
@@ -1,3 +1,7 @@
+"""
+This module initializes various extensions for the Form app.
+"""
+
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
@@ -9,7 +13,7 @@ csrf = CSRFProtect()
 # database
 database = SQLAlchemy()
 
-# A bootstrap5 class for styling client side. 
+# A bootstrap5 class for styling client sidpythe.
 bootstrap = Bootstrap5()
 
 # login manager
